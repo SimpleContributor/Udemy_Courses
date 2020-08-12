cc7e469705e81dd924cf8da0e6a2d659a4d61ad5
Caleb <67530577+SimpleContributor@users.noreply.github.com>
2020-08-11 22:16:12 -0600

    Build Files
    Added build files from the react components made in these exercises.

---
+++ build/precache-manifest.6e9031a50d1842961af5db25bd966d26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69cf1e502c9f604552eb284b031d50a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fe3c50c4a2a46dc402b",
+    "url": "/static/js/2.b7a8f9f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7a8f9f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa206e40bf6770f99221",
+    "url": "/static/js/main.323fb740.chunk.js"
+  },
+  {
+    "revision": "f0e9e0561a1081db1f31",
+    "url": "/static/js/runtime-main.0a541afb.js"
+  }
+]);
