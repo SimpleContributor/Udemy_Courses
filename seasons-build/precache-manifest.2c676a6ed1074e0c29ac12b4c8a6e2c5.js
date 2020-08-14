9bbf0ec3cf3375c5021a4c477bf618fa045eaf0b
Caleb <67530577+SimpleContributor@users.noreply.github.com>
2020-08-13 23:53:50 -0600

    Added season practice
    Created a simple app learning how to use state in React.

---
+++ seasons-build/precache-manifest.2c676a6ed1074e0c29ac12b4c8a6e2c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "095840a63e87d161f5205cf6ddbbccac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8038111ba34a38e76419",
+    "url": "/static/css/main.3170bb12.chunk.css"
+  },
+  {
+    "revision": "c2d246ecc2b83ccac6a6",
+    "url": "/static/js/2.8b77c9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8b77c9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8038111ba34a38e76419",
+    "url": "/static/js/main.5a71d9c1.chunk.js"
+  },
+  {
+    "revision": "8e73d7389c9fd7d72264",
+    "url": "/static/js/runtime-main.691d0382.js"
+  }
+]);
