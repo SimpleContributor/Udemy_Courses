48ddcfe571b5c07ac9ee1148c70f9bd151c5016b
Caleb <67530577+SimpleContributor@users.noreply.github.com>
2020-08-27 19:48:47 -0600

    Add files via upload
    Added pics practice

---
+++ pics-build/precache-manifest.5a2f5fae7b6ebdce96956b8c8bf80894.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eea1f890314061132956121a2bed638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac2abeb014559e2418ef",
+    "url": "/static/css/main.c7100457.chunk.css"
+  },
+  {
+    "revision": "18016f3c683fb5abffbe",
+    "url": "/static/js/2.5f8ad14d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f8ad14d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac2abeb014559e2418ef",
+    "url": "/static/js/main.01140641.chunk.js"
+  },
+  {
+    "revision": "b1820bfc6ade90caaf5b",
+    "url": "/static/js/runtime-main.51c4ed28.js"
+  }
+]);
