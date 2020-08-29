a0e9e56c251834fe859fc0605bc84fc8e1a4d1cb
Caleb <67530577+SimpleContributor@users.noreply.github.com>
2020-08-28 19:20:46 -0600

    Add files via upload
    Practice with youtube API and React

---
+++ videos-build/precache-manifest.a3ac879e0a4af31401b704c7b241dbcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370d068cd21118aa54f695b3c2bc969a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52e62b068b63f85779b4",
+    "url": "/static/css/main.b365c006.chunk.css"
+  },
+  {
+    "revision": "51e5b759ff5ca254900e",
+    "url": "/static/js/2.c87d17db.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/static/js/2.c87d17db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52e62b068b63f85779b4",
+    "url": "/static/js/main.448bf720.chunk.js"
+  },
+  {
+    "revision": "8c0ed676e7fc0e04cf11",
+    "url": "/static/js/runtime-main.81dc9e71.js"
+  }
+]);
